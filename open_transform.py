40ece8215c689cc0da07f4a0c0148ca185ace7c9
stoermerj <61618816+stoermerj@users.noreply.github.com>
2023-08-14 08:47:30 +0200

    Update open_transform.py

--- open_transform.py
+++ open_transform.py
@@ -5,6 +5,7 @@ import openpyxl
 from datetime import datetime
 import re
 
+#to change the path where e-mails are read from, simply change the string after path
 PATH = "example_mails"
 TODAY = datetime.today().strftime('%Y-%m-%d')
 
@@ -34,8 +35,6 @@ def open_transform_message(emails):
         body = msg.body #get body of email
         email_check = df.index[df['email']==receiver].tolist() #check if email address is in df
 
-        
-
         if "individuelle Konfiguration" in subject:
             if len(email_check) > 0:
                 df.at[email_check[0],'save_email'] = df.at[email_check[0],'save_email'] + 1
@@ -46,7 +45,6 @@ def open_transform_message(emails):
                 df.loc[len(df)] = new_data
 
         if "Ihr Angebot" in subject:
-            
             if len(email_check) > 0:
                 df.at[email_check[0],'get_quote_email'] = df.at[email_check[0],'get_quote_email'] + 1
                 df.at[email_check[0],'get_quote_time'] = received_time
@@ -69,9 +67,10 @@ def open_transform_message(emails):
 
 def total_payment(body):
      #find payment
-        payment = re.findall(r"\d+\s€\s", body)
+        payment = re.findall(r"\d+,\d+\s€", body)
         payment = re.split('\s', payment[0])
         payment = payment[0]
+        payment = payment.replace(',', '.')
         
         #find period of payment
         period = re.findall(r"(vierteljährlich|halbjährlich|jährlich|monatlich)", body)
@@ -86,9 +85,9 @@ def total_payment(body):
         
         #remove tax from payment
         if country == 'Germany':
-            net_payment = int(payment) / 1.19
+            net_payment = float(payment) / 1.19
         if country == 'Austria':
-            net_payment = int(payment) / 1.11
+            net_payment = float(payment) / 1.11
 
         #multiply by period
         if period == 'vierteljährlich':
@@ -100,13 +99,12 @@ def total_payment(body):
         if period == 'halbjährlich':
             period = 2
 
-
         total_payment = net_payment * period
         total_payment = round(total_payment, 2)
 
         #print(total_payment)
 
         return total_payment
-"""
-crawl = folder_crawler()
+
+"""crawl = folder_crawler()
 open_transform_message(crawl)"""
